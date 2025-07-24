28afab78db54bbfb09cae4734d5314a2efc634c0
xkacd1 <xkacd1@gmail.com>
2025-07-24 16:06:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-fetch('data/beers.json')
-  .then(response => response.json())
-  .then(beers => {
-    const container = document.getElementById('beer-menu');
-    beers.forEach(beer => {
-      const el = document.createElement('div');
-      el.innerHTML = `
-        <h2>${beer.name}</h2>
-        <p><strong>Style:</strong> ${beer.style} | <strong>ABV:</strong> ${beer.abv}</p>
-        <p>${beer.description}</p>
-      `;
-      container.appendChild(el);
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('data/beers.json')
+    .then(response => response.json())
+    .then(beers => {
+      const container = document.getElementById('beer-menu');
+      beers.forEach(beer => {
+        const el = document.createElement('div');
+        el.innerHTML = `
+          <h2>${beer.name}</h2>
+          <p><strong>Style:</strong> ${beer.style} | <strong>ABV:</strong> ${beer.abv}</p>
+          <p>${beer.description}</p>
+        `;
+        container.appendChild(el);
+      });
     });
-  });
+});
