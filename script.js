f6dfb6538c51bef8f0ae3968c7cfcb893286120b
xkacd1 <xkacd1@gmail.com>
2025-07-24 15:29:33 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch('data/beers.json')
+  .then(response => response.json())
+  .then(beers => {
+    const container = document.getElementById('beer-menu');
+    beers.forEach(beer => {
+      const el = document.createElement('div');
+      el.innerHTML = `
+        <h2>${beer.name}</h2>
+        <p><strong>Style:</strong> ${beer.style} | <strong>ABV:</strong> ${beer.abv}</p>
+        <p>${beer.description}</p>
+      `;
+      container.appendChild(el);
+    });
+  });
